63de541ba8c78d6e1fde310dba94af7324642652
hyeonss33 <hyeonsoo3413@gmail.com>
2020-05-10 19:40:55 +0900

    2주차장고실습

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lee hyeonsoo\desktop\멋사\assignment_hyeonsoo\django\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
